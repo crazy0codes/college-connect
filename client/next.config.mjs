91cb3585ba01505a9da98553b9be54ea27947eb8
crazy0codes <madhanpanja@gmail.com>
2025-05-11 14:44:05 +0530

    Remove API rewrite configuration from next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
-  async rewrites(){
-    return [
-      {
-        source: 'api/:auth*',
-        destination: 'https://4.240.92.168:3001/:auth*'
-      }
-    ]
-  },
-
   eslint: {
     ignoreDuringBuilds: true,
   },
